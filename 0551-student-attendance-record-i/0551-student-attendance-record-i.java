9a9d547841001adff9022349922477820e2753c1
AlpeshBorekar <alpeshborekar33@gmail.com>
2026-04-24 18:52:02 +0530

    Time: 2 ms (10.71%), Space: 42.8 MB (80.26%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean checkRecord(String s) {
+
+
+        int countA = 0 ;
+        int countL = 0 ;
+        boolean isconsecutive = false ;
+
+
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == 'A'){
+                countA++;
+            }
+            if(i < s.length() - 2 ){
+            if(s.charAt(i) == 'L' && s.charAt(i+1) == 'L' && s.charAt(i+2) == 'L'){
+                isconsecutive = true;
+            }
+            }
+        }
+        if(countA < 2 && isconsecutive == false){
+            return true;
+        }
+
+        return false;
+
+
+
+
+        
+    }
+}
