6fd0993f06eb1ba5e188349814251bef0003c0e9
AlpeshBorekar <alpeshborekar33@gmail.com>
2026-08-04 19:34:47 +0530

    Time: 2 ms (41.94%), Space: 94.6 MB (33.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,7 +4,7 @@ class Solution {
         int buy_price = prices[0];
         int profit = 0;
 
-        for(int i = 0 ; i < prices.length ; i++){
+        for(int i = 1 ; i < prices.length ; i++){
 
             if(prices[i] < buy_price){
                 buy_price = prices[i];
